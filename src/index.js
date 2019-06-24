ab65c913237ef73d317661e708b89d6365dfc0cc
Erin Millard <ezzatron@gmail.com>
2019-06-24 16:10:06 +1000

    Expose new path functions from main module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const {createConsumer} = require('./consumer.js')
+const {isAbsolutePath, relativePath, resolvePath} = require('./path.js')
 const {isAbsoluteUrl, relativeUrl, resolveUrl} = require('./url.js')
 const {readConsumer} = require('./reader.js')
 
 module.exports = {
   createConsumer,
+  isAbsolutePath,
   isAbsoluteUrl,
   readConsumer,
+  relativePath,
   relativeUrl,
+  resolvePath,
   resolveUrl,
 }
