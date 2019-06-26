ae427e3a8dc84db5610a7b8915b8ec14f8f097e2
Erin Millard <ezzatron@gmail.com>
2019-06-26 22:19:59 +1000

    Add some basic tests for consumer reading

---
+++ test/suite/consumer/read.spec.js
@@ -0,0 +1,48 @@
+const {expect} = require('chai')
+const {join, resolve} = require('path')
+
+const {readConsumer} = require('../../../src/reader.js')
+
+const fixtureDirPath = resolve(__dirname, '../../fixture')
+
+describe('readConsumer()', function () {
+  context('when the manifest is located directly in the output path', function () {
+    const fixturePath = join(fixtureDirPath, 'iconduit.flat.manifest.json')
+
+    beforeEach(function () {
+      this.consumer = readConsumer(fixturePath)
+    })
+
+    it('should produce the correct absolute path for document outputs', function () {
+      expect(this.consumer.absoluteDocumentPath('documentA')).to.equal(join(fixtureDirPath, 'document-a.html'))
+    })
+
+    it('should produce the correct relative path for document outputs', function () {
+      expect(this.consumer.documentPath('documentA')).to.equal('document-a.html')
+    })
+
+    it('should produce the correct absolute URL for document outputs', function () {
+      expect(this.consumer.absoluteDocumentUrl('documentA')).to.equal('https://iconduit.github.io/document-a.html')
+    })
+
+    it('should produce the correct relative URL for document outputs', function () {
+      expect(this.consumer.documentUrl('documentA')).to.equal('document-a.html')
+    })
+
+    it('should produce the correct absolute path for image outputs', function () {
+      expect(this.consumer.absoluteImagePath('imageA', 'sizeA')).to.equal(join(fixtureDirPath, 'image-a_size-a.png'))
+    })
+
+    it('should produce the correct relative path for image outputs', function () {
+      expect(this.consumer.imagePath('imageA', 'sizeA')).to.equal('image-a_size-a.png')
+    })
+
+    it('should produce the correct absolute URL for image outputs', function () {
+      expect(this.consumer.absoluteImageUrl('imageA', 'sizeA')).to.equal('https://iconduit.github.io/image-a_size-a.png')
+    })
+
+    it('should produce the correct relative URL for image outputs', function () {
+      expect(this.consumer.imageUrl('imageA', 'sizeA')).to.equal('image-a_size-a.png')
+    })
+  })
+})
